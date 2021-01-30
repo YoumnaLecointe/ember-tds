a551eaf3d4732374e5854668b9fff45e0ad950a9
lecointe <21804903@etu.unicaen.fr>
2021-01-30 18:16:08 +0100

    Ajout fichier

---
+++ app/controllers/ex1.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {tracked} from "@glimmer/tracking";
+
+
+export default class Ex1Controller extends Controller {
+}
