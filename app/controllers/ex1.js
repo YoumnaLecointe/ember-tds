a1e98b37177d0bf5a5ded088cff66009ea831e74
lecointe <21804903@etu.unicaen.fr>
2021-01-31 12:05:16 +0100

    Modification couleur style fonctionnel

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -8,14 +8,22 @@ export default class Ex1Controller extends Controller {
 
   MAX = 100;
 
-  get size(){
-    return this.MAX-this.content.length;
+  get size() {
+    return this.MAX - this.content.length;
   }
 
-  @action clear(){
+  // eslint-disable-next-line getter-return
+  get style() {
+    if (this.size > 50) {
+      return 'alert-warning';
+    } else if (this.size > 20) {
+      return 'alert-danger';
+    } else {
+      return 'alert-dark';
+    }
+  }
+
+  @action clear() {
     this.content = '';
   }
-
-
-
 }
