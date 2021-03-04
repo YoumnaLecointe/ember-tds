b263dac5e7df89ac99515bd4e750997e447bd8bd
lecointe <21804903@etu.unicaen.fr>
2021-03-04 23:12:10 +0100

    classe contact

---
+++ app/routes/test_new.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+export default class TestNewRoute extends Route {
+  model() {
+    return this.store.createRecord('contact', {
+      nom: 'Mimiiii',
+    });
+  }
+
+  @action save(contact) {
+    contact.save().then(() => {
+      this.transitionTo('testList');
+    });
+  }
+}
