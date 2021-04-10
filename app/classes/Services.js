f579ff7b67b22cbc9a2f0b2268d1a822e4120a50
lecointe <21804903@etu.unicaen.fr>
2021-04-10 16:28:44 +0200

    add formatpercent

--- app/classes/Services.js
+++ app/classes/Services.js
@@ -1,15 +1,22 @@
+import { tracked } from '@glimmer/tracking';
+
 export default class Services {
   services = [];
+  promo = [];
+  @tracked code = '';
 
-  constructor(serv) {
+  constructor(serv, promos) {
     this.services = serv;
+    this.promo = promos;
   }
+
   get countActive() {
     console.log(
       'Service actif : ' + this.services.filterBy('active', true).length
     );
     return this.services.filterBy('active', true).length;
   }
+
   get sumActive() {
     var resultat = 0;
     this.services.filterBy('active', true).forEach(function (service) {
@@ -18,4 +25,19 @@ export default class Services {
     return resultat;
   }
 
+  get promoValue() {
+    let check = document.getElementById('promoCheck');
+    let remise = document.getElementById('remise');
+    if (check.checked) {
+      remise.innerHTML = 'Code promo invalide';
+      if (this.code in this.promo) {
+        remise.innerHTML = 'Promo:' + this.promo[this.code] * this.sumActive + ' $';
+        return this.promo[this.code] * this.sumActive;
+      }
+    }
+    return 0;
+  }
+  get remise() {
+    return this.sumActive * this.promoValue;
+  }
 }
