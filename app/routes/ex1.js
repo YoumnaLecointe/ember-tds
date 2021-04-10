78071a4e3fdd105d8bef100876d9044a10c3a766
lecointe <21804903@etu.unicaen.fr>
2021-04-10 14:55:43 +0200

    some modif

--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -1,9 +1,13 @@
 import Route from '@ember/routing/route';
 import { datas } from 'tds/data/datas';
 import Services from 'tds/classes/Services';
+import { action, set } from '@ember/object';
 
 export default class Ex1Route extends Route {
   model() {
     return new Services(datas);
+  }
+  @action toggleActive(service) {
+    set(service, 'active', !service.active);
   }
 }
